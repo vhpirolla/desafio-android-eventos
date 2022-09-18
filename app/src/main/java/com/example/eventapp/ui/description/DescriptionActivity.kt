9c96aed95acc1bdeab2504b8eec69b34ef33ff40
vPirolla <vitorpirolla@gmail.com>
2022-09-18 16:57:27 -0300

    Share Event // Read More Event Description // Change App Icon

--- app/src/main/java/com/example/eventapp/ui/description/DescriptionActivity.kt
+++ app/src/main/java/com/example/eventapp/ui/description/DescriptionActivity.kt
@@ -1,12 +1,19 @@
 package com.example.eventapp.ui.description
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.FileProvider
+import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.example.eventapp.R
 import com.example.eventapp.data.remote.EventsModel
 import com.example.eventapp.databinding.ActivityDescriptionBinding
 import com.example.eventapp.ui.adapter.getAddress
+import java.io.File
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -18,6 +25,8 @@ class DescriptionActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDescriptionBinding
 
+    var eventTitle = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_description)
@@ -25,6 +34,13 @@ class DescriptionActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         loadEventFromExtra()
+
+        binding.ivButtonShare.setOnClickListener{
+            binding.ivButtonShare.isVisible = false
+            shareEvent()
+            binding.ivButtonShare.isVisible = true
+        }
+
     }
 
     private fun loadEventFromExtra() {
@@ -35,10 +51,9 @@ class DescriptionActivity : AppCompatActivity() {
             binding.lbEventDescriptionLocal.text = getAddress(this, it.latitude, it.longitude)
             binding.lbEventDescriptionDate.text = getDateFullDate(it.date)
             binding.lbEventDescriptionPrice.text = "R$ " + it.price
+            eventTitle = it.title
         }
     }
-
-    // TODO Read More Description
 
     fun getDateFullDate(s: String): String? {
         try {
@@ -49,4 +64,31 @@ class DescriptionActivity : AppCompatActivity() {
             return e.toString()
         }
     }
+
+    private fun shareEvent() {
+        val sdf = SimpleDateFormat("yyyy-mm-dd_hh:mm:ss")
+        val now = sdf.format(Date())
+        val path = getExternalFilesDir(null)!!.absolutePath +"/"+now +".jpg"
+        val bitmap = Bitmap.createBitmap(binding.lyActivityDescription.width, binding.lyActivityDescription.height, Bitmap.Config.ARGB_8888)
+        var canvas = Canvas(bitmap)
+        binding.lyActivityDescription.draw(canvas)
+        val imagefile=File(path)
+        val outputStream=FileOutputStream(imagefile)
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream)
+        outputStream.flush()
+        outputStream.close()
+
+        val URI = FileProvider.getUriForFile(applicationContext, "com.example.eventapp.android.fileprovider", imagefile)
+
+        val intent= Intent()
+        intent.action= Intent.ACTION_SEND
+        intent.putExtra(Intent.EXTRA_TEXT, eventTitle)
+        intent.putExtra(Intent.EXTRA_STREAM, URI)
+        intent.type="text/plain"
+        startActivity(intent)
+    }
+
+    // TODO Get all activity area
+
+
 }
